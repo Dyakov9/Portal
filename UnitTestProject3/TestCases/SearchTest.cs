93bd8056a71f725d853a7dbe031828cc4347be8e
Vadym.Diakov <Vadym.Diakov@3shape.com>
2017-02-09 17:04:43 +0200

    Worked with excel

---
+++ UnitTestProject3/TestCases/SearchTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PageObjects;
+using System.Configuration;
+using WrapperFactory;
+
+namespace TestCases
+{
+    [TestClass]
+    public class SearchTest
+    {
+     [TestMethod]
+         public static void Main()
+         {
+             BrowserFactory.InitBrowser();
+             BrowserFactory.LoadApplication(ConfigurationManager.AppSettings["URL"]);
+             Page.Login.LoginToAppliction("Test");
+             Page.IntegrationsPage.GoToCasePage();
+             Page.CasesPage.Search("Test");
+             BrowserFactory.QuitBrowser();
+         }
+    }
+}
