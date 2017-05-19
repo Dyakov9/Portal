349071860abff17db5f0934b06ec3f832018a05a
Vadym.Diakov <Vadym.Diakov@3shape.com>
2017-05-19 17:14:44 +0300

    SearchCasesAsAdmin

---
+++ UnitTestProject3/TestCases/AsAdminSearchCaseTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PageObjects;
+using System.Configuration;
+using WrapperFactory;
+
+
+namespace TestCases
+{
+    [TestClass]
+    public class AsAdminSearchCaseTest
+    {
+        [TestMethod]
+        public void AsAdminSearchCase()
+        {
+            BrowserFactory.InitBrowser();
+            BrowserFactory.LoadApplication(ConfigurationManager.AppSettings["URL"]);
+            Page.Login.LoginToAppliction(ConfigurationManager.AppSettings["AdminAccount"]);
+            Page.NavigationPage.GoToAdminPage();
+            Page.AdminPage.GoToUserSettingsPage(ConfigurationManager.AppSettings["ClinicAccount"]);
+            Page.NavigationPage.GoToCasesPage();
+            Page.CasesPage.Search();
+            BrowserFactory.QuitBrowser();
+            }
+    }
+}
